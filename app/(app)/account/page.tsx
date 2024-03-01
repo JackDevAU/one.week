29dce9d000afc6e6e7305d5bb7f5577aa3a6de6f
JackDevAU <jpr178@live.com.au>
2024-03-01 15:42:33 +1000

    add keystatic

--- app/(app)/account/page.tsx
+++ app/(app)/account/page.tsx
@@ -1,15 +1,22 @@
+import SignOutBtn from "@/components/auth/SignOutBtn";
 import UserSettings from "./UserSettings";
 import { checkAuth, getUserAuth } from "@/lib/auth/utils";
 
 export default async function Account() {
   await checkAuth();
   const { session } = await getUserAuth();
-  
+
   return (
     <main>
       <h1 className="text-2xl font-semibold my-4">Account</h1>
       <div className="space-y-4">
         <UserSettings session={session} />
+        <main className="">
+          <pre className="bg-secondary p-4 rounded-lg my-2">
+            {JSON.stringify(session, null, 2)}
+          </pre>
+          <SignOutBtn />
+        </main>
       </div>
     </main>
   );
---
+++ app/(app)/challenege/[slug]/page.tsx
@@ -0,0 +1,5 @@
+function ChallengePage(params: { params: { slug: string } }) {
+  return <div>Enter</div>;
+}
+
+export default ChallengePage;
